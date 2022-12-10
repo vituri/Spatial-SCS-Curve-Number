6cc8df9da393b975894c623946ee1e8b9fdf5c8e
Lucas Vituri Santarosa <60663771+lvsantarosa@users.noreply.github.com>
2022-12-10 10:24:52 -0300

    Update Curve-number-SCSmethod.R

--- Curve-number-SCSmethod.R
+++ Curve-number-SCSmethod.R
@@ -3,63 +3,27 @@
 #(https://www.nrcs.usda.gov/Internet/FSE_DOCUMENTS/stelprdb1044171.pdf)
 
 #Data:
-  #Land use from the Mapbiomas Landsat classification collection, from GEE: https://mapbiomas.org/
-  
+#Land use from the Mapbiomas Landsat classification collection, from GEE: https://mapbiomas.org/
+
 library(terra)    
 
 dir.create(path = 'temp', showWarnings = FALSE) #To create a temp directory to exclude in the final
 
 terraOptions(tempdir = 'temp')
 
-lista <- list.files('MAPBIOMAS-EXPORT', full.names = TRUE, pattern = "saopaulo")
 
-uses_orig = terra::rast(lista)
-
-#Can be adjusted for an area based on interest
-grid_standard = terra::rast(xmin=-49.270, xmax =-48.843, ymin =-21.490, ymax =-21.147, resolution = c(0.001, 0.001), vals = 0)
-
-#Reduce the amount of data for easier processing, adjustable as needed (grid). 
-mapbio_resample = terra::resample(uses_orig, grid_standard, method="near") 
-
-# Values reclass/Land use	----- (MAPBIOMAS) 
-# There are different classes depending on the collection. 
-
-#10	Urban (24)
-#20	Agriculture (18, 19, 20, 21, 36, 39, 41)
-#30	Forest (3, 4, 5)
-#40	Silviculture (9)
-#50	Grassland  (10, 11, 12, 13, 32)
-#60	Uncover (23,25, 29, 30)
-#70	Pasture (15)
-#100 Water (27,31,33)
-
-
-# multiple replacements
-class_map_scs <- rbind(c(24, 10), c(20, 20), c(21, 20), c(36, 20), c(39, 20), c(41, 20),
-                       c(3, 30), c(4, 30), c(5, 30), c(9, 40), c(11, 50), c(12, 50), 
-                       c(13, 50), c(32, 50), c(23, 60), c(25, 60), c(29, 60), c(30, 60),
-                       c(15, 70), c(31, 100), c(33, 100))
-
-
-#Reclassify uses based in the matrix
-rc_map_scs = classify(mapbio_resample, class_map_scs)
-
-#Remove 0 
-rc_map_scs[rc_map_scs <= 0] = NA
-
-#plot(rc_map_scs$classification_2000, type = "classes")
-
-
-################### Soil raster ###############
+#########################################################################
+############################# Soil raster ###############################
+#########################################################################
 
 #SCS Soil group from SCS method	A = (1)	B = (2)	C = (3)	D = (4) 
 # Layers from Soil Grid
 # See my script to download in Google Earth Engine
 # https://github.com/lvsantarosa/Soil-Grid-on-Google-Earth-Engine
 
-Clay <- terra::rast('clay.tif')
-Sand <- terra::rast('sand.tif')
-Silt <- terra::rast('silt.tif')
+Clay <- terra::rast('Soil/clay.tif')
+Sand <- terra::rast('Soil/sand.tif')
+Silt <- terra::rast('Soil/silt.tif')
 
 #Convert to percentage
 
@@ -95,7 +59,7 @@ Sum_ly <- Sand_rec + Clay_rec + Silt_rec
 #Discriminate the groups based in the sum result
 grupos <- rbind(c(1110, 1),
                 c(1220, 1),
-                c(1310, 1),
+                c(1310, 2),
                 c(1120, 2),
                 c(2120, 2),
                 c(1210, 3),
@@ -106,17 +70,49 @@ Soil_Hidro <- classify(Sum_ly, grupos)
 crs <- "+proj=longlat +datum=WGS84"
 Soil_Hidro <- project(Soil_Hidro, crs)
 
-#terra::writeRaster(Soil_Hidro, "Soil_Hidro.tif", overwrite = TRUE)
+Soil_Hidro[Soil_Hidro == 0] = NA
 
-Soil_resample= resample(Soil_Hidro, grid_standard, method="near")
 
-Soil_resample[Soil_resample <= 0] = NA
+#########################################################################
+################################## LULC #################################
+#########################################################################
 
-#plot(Soil_resample)
+lista <- list.files('LULC/', full.names = TRUE, pattern = '.tif$')
 
-sum_soil_uses = Soil_resample + rc_map_scs
+uses_orig = terra::rast(lista)
 
-#plot(sum_soil_uses$lyr1, type = "classes")
+#Reduce the amount of data for easier processing, adjustable as needed (grid). 
+mapbio_resample = terra::resample(uses_orig, Soil_Hidro, method="near") 
+
+# MAPBIOMAS land use values to be reclassified as SCS method
+# There are different classes depending on the MAPBIOMAS collection. 
+
+#10	Urban (24)
+#20	Agriculture (14, 18, 19, 20, 21, 36, 39, 40, 41, 46, 47, 48, 62)
+#30	Forest (1, 3, 4, 5, 49)
+#40	Silviculture (9)
+#50	Grassland  (12, 13, 32, 50)
+#60	Uncover (23,25, 29, 30)
+#70	Pasture (15)
+#100 Water (10, 27,31,33)
+
+
+# multiple replacements
+class_map_scs <- rbind(c(24, 10), 
+                       c(14, 20),c(18, 20), c(19, 20), c(20, 20), c(21, 20), c(36, 20), c(39, 20), c(40, 20), c(41, 20), c(46, 20), c(47, 20), c(62, 20), 
+                       c(1, 30), c(3, 30), c(4, 30), c(5, 30), c(49, 30), 
+                       c(9, 40), 
+                       c(12, 50), c(13, 50), c(32, 50), c(50, 50), 
+                       c(23, 60), c(25, 60), c(29, 60), c(30, 60),
+                       c(15, 70),
+                       c(11, 100), c(27, 100), c(31, 100), c(33, 100))
+
+
+#Reclassify uses based in the matrix
+rc_map_scs = classify(mapbio_resample, class_map_scs)
+
+#Remove 0 
+rc_map_scs[rc_map_scs <= 0] = NA
 
 
 #####################CN final##########################
@@ -155,15 +151,17 @@ class_cn <- rbind(c(11,89), c(12,64), c(13,32), c(14,32), c(21,48),
                   c(103,0), c(104,0))
 
 #Reclassify
+
+sum_soil_uses = Soil_Hidro + rc_map_scs
+
 cn_final = classify(sum_soil_uses, class_cn)
 
 #Names
-Nomes = seq(from = 2019, to = 2020)
+Nomes = seq(from = 2000, to = 2021)
 names(cn_final) = Nomes
 
 cn_final[cn_final <= 0] = NA
 
-#plot(cn_final$`2020`, type = "classes")
 
 library(glue)
 library(dplyr)
@@ -172,5 +170,12 @@ library(dplyr)
 filename = glue("Results/CN_{Nomes}.tif")
 cn_final %>% writeRaster(filename = filename, overwrite = TRUE)
 
+#Export soil layer
+Soil_Hidro <- mask(Soil_Hidro, cn_final[[1]])
+terra::writeRaster(Soil_Hidro, "Results/Soil_Hidro.tif", overwrite = TRUE)
+
 ###########################################################
-unlink(x = list.files('temp', full.names = TRUE)) #Delete the temp
+unlink(x = list.files('temp', full.names = TRUE)) #Delete the temp archives
+
+
+
